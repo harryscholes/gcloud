f0ec71e408a7167f25d826e74127d879cf63316d
harryscholes <harryscholes@gmail.com>
2018-08-09 15:57:42 +0100

    Fix sourcing .bash_profile

--- agape/setup.sh
+++ agape/setup.sh
@@ -1,18 +1,17 @@
-# Google ML VM Base Cuda 9.0
-
 # Setup .bash_profile
-rm -f ~/.bash_profile && ln -s $PWD/.bash_profile $HOME/.bash_profile && source $HOME/.bash_profile
+rm -f ~/.bash_profile && ln -s $PWD/.bash_profile $HOME/.bash_profile && source ~/.bash_profile
 
 # Install packages
 curl -O https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh \
   && bash Anaconda3-5.2.0-Linux-x86_64.sh -f -b -p $HOME/anaconda \
   && echo "export PATH=~/anaconda/bin:${PATH}" >> ~/.bash_profile \
-  && refresh \
+  && source ~/.bash_profile \
   && rm Anaconda3-5.2.0-Linux-x86_64.sh \
   && conda config --add channels conda-forge \
-  && conda install nb_conda_kernels
+  && conda install nb_conda_kernels --yes
 pip install --upgrade pip \
-  && pip install tensorflow-gpu keras
+  && pip install tensorflow-gpu keras \
+  && python cuda_test.py
 # Ref: https://towardsdatascience.com/running-jupyter-notebook-in-google-cloud-platform-in-15-min-61e16da34d52
 jupyter notebook --generate-config
 cat jupyter_config.py >> ~/.jupyter/jupyter_notebook_config.py
@@ -20,6 +19,6 @@ cat jupyter_config.py >> ~/.jupyter/jupyter_notebook_config.py
 # Setup filesystem
 mkdir ~/git && git clone https://github.com/harryscholes/agape.git && mv agape ~/git
 (cd ~/git/agape && pip install -e ".[gpu]")
-mkdir -p lfs/cerevisiae/deepNF
-echo "export CEREVISIAEDATA=~/lfs/cerevisiae" >> ~/.bash_profile && refresh
+mkdir -p ~/lfs/cerevisiae/deepNF
+echo "export CEREVISIAEDATA=~/lfs/cerevisiae" >> ~/.bash_profile && source ~/.bash_profile
 gsutil cp -r gs://cerevisiae/deepNF $CEREVISIAEDATA
