5eb33fe55544adcb459f7e715bb38a250eee2e08
harryscholes <harryscholes@gmail.com>
2018-08-09 15:42:52 +0100

    Fix relative paths

--- agape/setup.sh
+++ agape/setup.sh
@@ -1,12 +1,12 @@
 # Google ML VM Base Cuda 9.0
 
 # Setup .bash_profile
-rm -f ~/.bash_profile && ln -s .bash_profile $HOME/.bash_profile && source .bash_profile
+rm -f ~/.bash_profile && ln -s $PWD/.bash_profile $HOME/.bash_profile && source $HOME/.bash_profile
 
 # Install packages
 curl -O https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh \
   && bash Anaconda3-5.2.0-Linux-x86_64.sh -f -b -p $HOME/anaconda \
-  && echo "export PATH=~/anaconda/bin:${PATH}" >> .bash_profile \
+  && echo "export PATH=~/anaconda/bin:${PATH}" >> ~/.bash_profile \
   && refresh \
   && rm Anaconda3-5.2.0-Linux-x86_64.sh \
   && conda config --add channels conda-forge \
@@ -21,5 +21,5 @@ cat jupyter_config.py >> ~/.jupyter/jupyter_notebook_config.py
 mkdir ~/git && git clone https://github.com/harryscholes/agape.git && mv agape ~/git
 (cd ~/git/agape && pip install -e ".[gpu]")
 mkdir -p lfs/cerevisiae/deepNF
-echo "export CEREVISIAEDATA=~/lfs/cerevisiae" >> .bash_profile && refresh
+echo "export CEREVISIAEDATA=~/lfs/cerevisiae" >> ~/.bash_profile && refresh
 gsutil cp -r gs://cerevisiae/deepNF $CEREVISIAEDATA
